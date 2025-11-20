9493fbd920b28c64aae04c9d526252787729617b
ARIEFZ23 <127678675+ARIEFZ23@users.noreply.github.com>
2025-11-20 15:37:55 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            // 1. User (Admin, Ustadz, Santri) WAJIB dibuat pertama
+            UserSeeder::class,
+
+            // 2. Game Survival
             DummySurvivalGameSeeder::class,
             SurvivalQuestionsSeeder::class,
+
+            // 3. Game Sentence Builder
             SentenceBuilderGameSeeder::class,
             SentenceBuilderQuestionsSeeder::class,
+
+            // 4. Game Listening (Audio)
             ListeningAudioSeeder::class,
         ]);
     }
